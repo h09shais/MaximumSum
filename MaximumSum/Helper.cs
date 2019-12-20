c4e44b7b1b9b985a8bac843c0ccd5ea9aecae350
Shanawaz Islam <sis@nisportal.com>
2019-12-20 10:22:04 +0100

    Caught exceptions and refactoring

--- MaximumSum/Helper.cs
+++ MaximumSum/Helper.cs
@@ -8,21 +8,39 @@
         {
             using var reader = new StreamReader(filename);
             var row = 0;
-            var length = int.Parse(reader.ReadLine());
+            var length = int.Parse(reader.ReadLine() ?? throw new InvalidOperationException("Bad data!"));
 
-            var matrix = new int[length, length];
+            var collection = new int[length, length];
 
             while (!reader.EndOfStream)
             {
-                var tokens = reader.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var tokens = reader.ReadLine()?.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (tokens == null) continue;
 
                 for (var i = 0; i < tokens.Length; i++)
-                    matrix[row, i] = int.Parse(tokens[i]);
+                {
+                    collection[row, i] = int.Parse(tokens[i]);
+                }
 
                 row++;
             }
 
-            return matrix;
+            return collection;
+        }
+
+        public void Display(int[,] collection)
+        {
+            var collectionSize = collection.GetLength(0);
+
+            for (var i = 0; i < collectionSize; i++)
+            {
+                for (var j = 0; j < collectionSize; j++)
+                {
+                    Console.Write(collection[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
         }
     }
 }
--- MaximumSum/Program.cs
+++ MaximumSum/Program.cs
@@ -1,9 +1,11 @@
 ﻿namespace MaximumSum
 {
     using System;
+
     class Program
     {
         public Helper Helper { get; } = new Helper();
+
         static void Main(string[] args)
         {
             new Program().GetMaximumSum();
@@ -11,70 +13,57 @@
 
         public void GetMaximumSum()
         {
-            var grid = Helper.ReadInput("../../Matrix.txt");
-            var gridSize = grid.GetLength(0);
-
-            for (var i = gridSize - 2; i >= 0; i--)
+            try
             {
-                for (var j = 0; j <= i; j++)
+                var collection = Helper.ReadInput("../../Matrix.txt");
+                var gridSize = collection.GetLength(0);
+
+                for (var i = gridSize - 2; i >= 0; i--)
                 {
-                    var seed = new Node { Weight = grid[i, j] };
-                    var child1 = new Node { Weight = grid[i + 1, j] };
-                    var child2 = new Node { Weight = grid[i + 1, j + 1] };
-
-                    if (child1.IsEven && child2.IsEven && seed.IsEven)
+                    for (var j = 0; j <= i; j++)
                     {
-                        grid[i, j] = 0;
-                    }
+                        var seed = new Node {Weight = collection[i, j]};
+                        var child1 = new Node {Weight = collection[i + 1, j]};
+                        var child2 = new Node {Weight = collection[i + 1, j + 1]};
 
-                    else if (!child1.IsEven && !child2.IsEven && !seed.IsEven)
-                    {
-                        grid[i, j] = 0;
-                    }
-                    else
-                    {
-                        var maxWeight = getMaxWeight(seed, child1, child2);
-                        seed.Weight += maxWeight;
-                        grid[i, j] = seed.Weight;
-                    }
+                        if (child1.IsEven && child2.IsEven && seed.IsEven)
+                        {
+                            collection[i, j] = 0;
+                        }
 
-                    PrintGrid(grid);
+                        else if (!child1.IsEven && !child2.IsEven && !seed.IsEven)
+                        {
+                            collection[i, j] = 0;
+                        }
+                        else
+                        {
+                            var maxWeight = getMaxWeight(seed, child1, child2);
+                            seed.Weight += maxWeight;
+                            collection[i, j] = seed.Weight;
+                        }
+
+                        Helper.Display(collection);
+                    }
                 }
-            }
 
-            Console.WriteLine();
-            Console.WriteLine("Max sum: {0}", grid[0, 0]);
-            Console.WriteLine("Path:");
+                Console.WriteLine();
+                Console.WriteLine("Max sum: {0}", collection[0, 0]);
+                Console.WriteLine("Path:");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
         }
 
         private int getMaxWeight(Node seed, Node child1, Node child2)
         {
-            if (child1.IsEven && child2.IsEven)
-            {
-                return Math.Max(child1.Weight, child2.Weight);
-            }
-
-            if (!child1.IsEven && !child2.IsEven)
+            if (child1.IsEven && child2.IsEven || !child1.IsEven && !child2.IsEven)
             {
                 return Math.Max(child1.Weight, child2.Weight);
             }
 
             return child1.IsEven && !seed.IsEven || child2.Weight == 0 ? child1.Weight: child2.Weight;
-        }
-
-        private void PrintGrid(int[,] grid)
-        {
-            var gridSize = grid.GetLength(0);
-
-            for (var i = 0; i < gridSize; i++)
-            {
-                for (var j = 0; j < gridSize; j++)
-                {
-                    Console.Write(grid[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
         }
     }
 }
