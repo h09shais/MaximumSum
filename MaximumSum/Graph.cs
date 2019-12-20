c98b1b2f2e2d2dc431170c88ed9e32121fbe98ba
Shanawaz Islam <sis@nisportal.com>
2019-12-20 11:08:18 +0100

    Refactor to Graph

---
+++ MaximumSum/Graph.cs
@@ -0,0 +1,62 @@
+﻿namespace MaximumSum
+{
+    using System;
+
+    public class Graph
+    {
+        private readonly int[,] _collection;
+
+        private int CollectionSize => _collection.GetLength(0);
+
+        public Graph(int[,] collection)
+        {
+            this._collection = collection;
+        }
+
+        public int GetMaximumSum()
+        {
+            for (var i = CollectionSize - 2; i >= 0; i--)
+            {
+                for (var j = 0; j <= i; j++)
+                {
+                    var seed = new Node { Weight = _collection[i, j] };
+                    var child1 = new Node { Weight = _collection[i + 1, j] };
+                    var child2 = new Node { Weight = _collection[i + 1, j + 1] };
+
+                    if (child1.IsEven && child2.IsEven && seed.IsEven)
+                    {
+                        _collection[i, j] = 0;
+                    }
+
+                    else if (!child1.IsEven && !child2.IsEven && !seed.IsEven)
+                    {
+                        _collection[i, j] = 0;
+                    }
+                    else
+                    {
+                        var maxWeight = getMaxWeight(seed, child1, child2);
+                        seed.Weight += maxWeight;
+                        _collection[i, j] = seed.Weight;
+                    }
+                }
+            }
+
+            return _collection[0, 0];
+        }
+
+        public string GetMaximumSumPath()
+        {
+            return string.Empty;
+        }
+
+        private int getMaxWeight(Node seed, Node child1, Node child2)
+        {
+            if (child1.IsEven && child2.IsEven || !child1.IsEven && !child2.IsEven)
+            {
+                return Math.Max(child1.Weight, child2.Weight);
+            }
+
+            return child1.IsEven && !seed.IsEven || child2.Weight == 0 ? child1.Weight : child2.Weight;
+        }
+    }
+}
--- MaximumSum/Program.cs
+++ MaximumSum/Program.cs
@@ -11,59 +11,21 @@
             new Program().GetMaximumSum();
         }
 
-        public void GetMaximumSum()
+        private void GetMaximumSum()
         {
             try
             {
                 var collection = Helper.ReadInput("../../Matrix.txt");
-                var gridSize = collection.GetLength(0);
-
-                for (var i = gridSize - 2; i >= 0; i--)
-                {
-                    for (var j = 0; j <= i; j++)
-                    {
-                        var seed = new Node {Weight = collection[i, j]};
-                        var child1 = new Node {Weight = collection[i + 1, j]};
-                        var child2 = new Node {Weight = collection[i + 1, j + 1]};
-
-                        if (child1.IsEven && child2.IsEven && seed.IsEven)
-                        {
-                            collection[i, j] = 0;
-                        }
-
-                        else if (!child1.IsEven && !child2.IsEven && !seed.IsEven)
-                        {
-                            collection[i, j] = 0;
-                        }
-                        else
-                        {
-                            var maxWeight = getMaxWeight(seed, child1, child2);
-                            seed.Weight += maxWeight;
-                            collection[i, j] = seed.Weight;
-                        }
-
-                        Helper.Display(collection);
-                    }
-                }
-
+                var graph = new Graph(collection);
+                
                 Console.WriteLine();
-                Console.WriteLine("Max sum: {0}", collection[0, 0]);
-                Console.WriteLine("Path:");
+                Console.WriteLine("Max sum: {0}", graph.GetMaximumSum());
+                Console.WriteLine("Path: {0}", graph.GetMaximumSumPath());
             }
             catch (Exception exception)
             {
                 Console.WriteLine(exception.Message);
             }
-        }
-
-        private int getMaxWeight(Node seed, Node child1, Node child2)
-        {
-            if (child1.IsEven && child2.IsEven || !child1.IsEven && !child2.IsEven)
-            {
-                return Math.Max(child1.Weight, child2.Weight);
-            }
-
-            return child1.IsEven && !seed.IsEven || child2.Weight == 0 ? child1.Weight: child2.Weight;
         }
     }
 }
