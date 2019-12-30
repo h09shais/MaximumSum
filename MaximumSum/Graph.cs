72d3211eb7c4eb662c8bb7dc6aad0a50b7c5f6d0
Shanawaz Islam <sis@nisportal.com>
2019-12-30 11:10:41 +0100

    Added node stack

--- MaximumSum/Graph.cs
+++ MaximumSum/Graph.cs
@@ -3,20 +3,38 @@
     using System;
     using System.Collections.Generic;
 
-    public class Graph: IGraph
+    public class Graph : IGraph
     {
         private readonly int[,] _collection;
 
-        private readonly Stack<int> _stack;
+        private readonly int[] _flattenCollection;
+
+        private readonly Stack<Node> _stack;
 
         private int CollectionSize => _collection.GetLength(0);
 
         public Graph(int[,] collection)
         {
             this._collection = collection;
-            this._stack = new Stack<int>();
+            this._flattenCollection = RowMajorOrderFlatten(collection);
+            this._stack = new Stack<Node>();
         }
 
+        private int[] RowMajorOrderFlatten(int[,] collection)
+        {
+            var k = 0;
+            var flattenCollection = new int[CollectionSize * CollectionSize];
+            for (var i = 0; i < CollectionSize; i++)
+            {
+                for (var j = 0; j < CollectionSize; j++)
+                {
+                    flattenCollection[k++] = collection[i, j];
+                }
+            }
+
+            return flattenCollection;
+        }
+        
         public int MaximumSum => CalculatedSum();
 
         private int CalculatedSum()
@@ -25,38 +43,69 @@
             {
                 for (var j = 0; j <= i; j++)
                 {
-                    var seed = new Node {Weight = _collection[i, j]};
-                    var child1 = new Node {Weight = _collection[i + 1, j]};
-                    var child2 = new Node {Weight = _collection[i + 1, j + 1]};
+                    var seed = new Node {Weight = _collection[i, j], XCoordinate = i, YCoordinate = j};
+                    var child1 = new Node {Weight = _collection[i + 1, j], XCoordinate = i + 1, YCoordinate = j};
+                    var child2 = new Node {Weight = _collection[i + 1, j + 1], XCoordinate = i + 1, YCoordinate = j + 1};
 
-                    var maxWeight = GetMaxWeight(seed, child1, child2);
-                    if (maxWeight == null)
-                    {
-                        continue;
-                    }
-
-                    var weight = maxWeight.Value;
-                    _stack.Push(weight);
-                    seed.Weight += weight;
+                    var node = GetMaxWeight(seed, child1, child2);
+                    seed.Weight += node.Weight;
+                    _stack.Push(node);
                     _collection[i, j] = seed.Weight;
                 }
             }
 
-            return _collection[0, 0];
+            //_stack.Push(new Node { XCoordinate = 0, YCoordinate = 0 });
+            var root = _collection[0, 0];
+            return root;
         }
 
         public string MaximumSumPath => CalculatedPath();
 
+        private void AddPath(Node node)
+        {
+            var index = GetIndex(node);
+            var item = GetItem(index);
+            //_stack.Push(item);
+        }
+
+        private int GetItem(int index)
+        {
+            var item = _flattenCollection[index];
+            return item;
+        }
+
+        private int GetIndex(Node node)
+        {
+            var index = node.XCoordinate * CollectionSize + node.YCoordinate;
+            return index;
+        }
+
         private string CalculatedPath()
         {
-            var result = string.Join(" -> ", _stack.ToArray());
+            var items = new List<int>();
+            foreach (var item in _stack)
+            {
+                var index = GetIndex(item);
+                var element = GetItem(index);
+                items.Add(element);
+            }
+
+            //for (var i = 0; i < CollectionSize - 1; i++)
+            //{
+            //    for (var j = 0; j < i; j++)
+            //    {
+
+            //    }
+            //}
+
+            var result = string.Join(", ", items);
             return result;
         }
 
-        private int? GetMaxWeight(Node seed, Node child1, Node child2)
+        private Node GetMaxWeight(Node seed, Node child1, Node child2)
         {
             // TODO: Even - Odd choices
-            return Math.Max(child1.Weight, child2.Weight);
+            return Math.Max(child1.Weight, child2.Weight) == child1.Weight? child1: child2;
         }
     }
 }
--- MaximumSum/Node.cs
+++ MaximumSum/Node.cs
@@ -4,6 +4,10 @@
     {
         public int Weight { get; set; }
 
+        public int XCoordinate { get; set; }
+
+        public int YCoordinate { get; set; }
+        
         public bool IsEven => Weight % 2 == 0;
     }
 }
