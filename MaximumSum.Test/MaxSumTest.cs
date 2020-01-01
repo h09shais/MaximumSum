a8a6de595779e02763da234d7fc21534af9de49f
Shanawaz Islam <sis@nisportal.com>
2020-01-01 20:11:06 +0100

    Added another max sum test

--- MaximumSum.Test/MaxSumTest.cs
+++ MaximumSum.Test/MaxSumTest.cs
@@ -23,6 +23,25 @@
             }
         }
 
+        public static TheoryData<string[]> Data2
+        {
+            get
+            {
+                var data = new[]
+                {
+                    "99",
+                    "12 67",
+                    "33 45 66",
+                    "23 56 91 74",
+                    "56 83 12 56 77",
+                    "56 88 23 45 71 34",
+                    "45 87 23 56 88 11 45"
+                };
+
+                return new TheoryData<string[]> { data };
+            }
+        }
+
         [Theory]
         [MemberData(nameof(Data1))]
         public void ShouldGetMaximumSumSixteen(string[] data)
@@ -34,6 +53,19 @@
             var graph = A.Fake<Graph>(src => src.WithArgumentsForConstructor(() => new Graph(reader)));
 
             graph.MaxSum.Should().Be(16);
+        }
+
+        [Theory]
+        [MemberData(nameof(Data2))]
+        public void ShouldGetMaximumSumFourHundredSeventy(string[] data)
+        {
+            var repository = A.Fake<IGraphRepository>();
+            A.CallTo(() => repository.GetGraph()).Returns(data);
+
+            var reader = A.Fake<DataReader>(src => src.WithArgumentsForConstructor(() => new DataReader(repository)));
+            var graph = A.Fake<Graph>(src => src.WithArgumentsForConstructor(() => new Graph(reader)));
+
+            graph.MaxSum.Should().Be(470);
         }
     }
 }
